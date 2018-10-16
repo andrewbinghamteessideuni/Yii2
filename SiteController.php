473ceaf4c062f96c73d79d15109c8da6663bf639
Dr Andrew Bingham <a.p.bingham@tees.ac.uk>
2018-10-16 09:52:11 +0100

    Update SiteController.php
    added UploadedFile class

--- SiteController.php
+++ SiteController.php
@@ -5,6 +5,9 @@ use app\models\Properties;
 use app\models\Images;
 use app\models\PropertiesImages;
 
+//bring in the UploadedFile class -- this reveals information about the file
+use yii\web\UploadedFile;
+
 
 
 
